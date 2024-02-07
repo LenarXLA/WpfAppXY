fd953348ad931ec93f68119143eeb5051562e21b
LenarXLA <leonruscraft@gmail.com>
2024-02-07 18:59:17 +0500

    не прогрузился xaml

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfAppXY.ViewModel;
 
 namespace WpfAppXY
 {
@@ -23,6 +24,9 @@ namespace WpfAppXY
         public MainWindow()
         {
             InitializeComponent();
+            
+            // Передаем DataContext
+            DataContext = new MainVM();
         }
     }
 }
